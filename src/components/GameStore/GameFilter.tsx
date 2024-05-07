8675fcd0a38ac7dcfe11ca94e5f8cb936f22dd94
SeifAbdAlAzem <156600890+SeifAbdAlAzem@users.noreply.github.com>
2024-05-07 20:32:42 +0300

    Build Platform Selector

--- src/components/GameStore/GameFilter.tsx
+++ src/components/GameStore/GameFilter.tsx
@@ -1,18 +1,30 @@
-import { HStack, Select } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Select,
+} from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatform from "../../hooks/usePlatform";
 
 const GameFilter = () => {
+  const { data } = usePlatform();
+
   return (
     <HStack spacing={2} my={5}>
-      <Select
-        placeholder="Platforms"
-        aria-label="Platforms"
-        variant="filled"
-        w="fit-content"
-      >
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
+      <Menu aria-label="Platforms" variant="filled">
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          Select Platform
+        </MenuButton>
+        <MenuList>
+          {data.map((platform) => (
+            <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
       <Select
         placeholder="Order by: Relevance"
         aria-label="Order"
