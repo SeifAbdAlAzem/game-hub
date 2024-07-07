19488de4af4678eab42cd39f8b256b41a7c15788
SeifAbdAlAzem <seif.abdalazemweb@gmail.com>
2024-07-07 20:25:10 +0300

    Refactoring - simplify time calculation in staleTime in custom hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
+import ms from "ms";
 
 export interface GamePlatform {
     id: number;
@@ -16,7 +17,7 @@ const usePlatforms = () => {
         useQuery({
             queryKey: ['platforms'],
             queryFn: apiClient.getAll,
-            staleTime: 24 * 60 * 60 * 1000,
+            staleTime: ms('24h'),
             initialData: {count: platforms.length, next: null, results: platforms}
         })
     )
